6608e8aec217032d78a07521569fbfe39d51dac6
Kaslana <rafidzakwan179@gmail.com>
2026-04-07 16:18:33 +0700

    Upload tugas akhir web pertama

---
+++ Sejarah.js
@@ -0,0 +1,21 @@
+tailwind.config = {
+    darkMode: "class",
+    theme: {
+        extend: {
+            colors: {
+                "primary": "#028bd4",
+                "background-light": "#f5f7f8",
+                "background-dark": "#0f1c23",
+            },
+            fontFamily: {
+                "display": ["Public Sans", "sans-serif"]
+            },
+            borderRadius: {
+                "DEFAULT": "0.25rem", 
+                "lg": "0.5rem", 
+                "xl": "0.75rem", 
+                "full": "9999px"
+            },
+        },
+    },
+}
